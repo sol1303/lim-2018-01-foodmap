faead54f7ab921eb3d78785044763a3679e2391a
Sol Lavarello <lavarellosolange@gmail.com>
2018-08-03 05:40:45 -0500

    modal casi listos

--- app.js
+++ app.js
@@ -2,10 +2,45 @@
 $('.ui.accordion')
   .accordion();
 
+$(function () {
+  $("#test").click(function () {
+    $(".test").modal('show');
+  });
+  $(".test").modal({
+    closable: true
+  });
+});
+
 const restaurantTable = document.getElementById('restaurant-table');
 const searchByName = document.getElementById('search-restaurant');
-const amazonRest = document.getElementById('amazon-restaurants'); 
+const amazonRest = document.getElementById('amazon-restaurants');
 const tableAmazon = document.getElementById('amazon-table');
+const americanRest = document.getElementById('american-restaurants');
+const tableAmerican = document.getElementById('american-table');
+const meatAndGrillRest = document.getElementById('meatandgrill-restaurants');
+const tableMeatAndGrill = document.getElementById('meatandgrill-table');
+const chifaRest = document.getElementById('chifa-restaurants');
+const tableChifa = document.getElementById('table-chifa');
+const fusionRest = document.getElementById('fusion-restaurant');
+const tableFusion = document.getElementById('table-fusion');
+const indiaRest = document.getElementById('india-restaurant');
+const tableIndia = document.getElementById('table-india');
+const internationalRest = document.getElementById('international-restaurant');
+const tableInternational = document.getElementById('table-international');
+const italianRest = document.getElementById('italian-restaurant');
+const tableItalian = document.getElementById('table-italian');
+const nikkeiRest = document.getElementById('nikkei-restaurant');
+const tableNikkei = document.getElementById('table-nikkei');
+const peruvianRest = document.getElementById('peruvian-restaurant');
+const tablePeruvian = document.getElementById('table-peruavian');
+const fishRest = document.getElementById('fish-restaurant');
+const tableFish = document.getElementById('table-fish');
+const snackRest = document.getElementById('snack-restaurant');
+const tableSnack = document.getElementById('table-snack');
+const pizzaRest = document.getElementById('pizza-restaurant');
+const tablePizza = document.getElementById('table-pizza');
+const thaiRest = document.getElementById('thai-restaurant');
+const tableThai = document.getElementById('table-thai');
 
 const restaurantJson = () => {
   let xhr = new XMLHttpRequest();
@@ -24,7 +59,7 @@ const listRestaurant = (xhrRestaurant) => {
   for (const rest of xhrRestaurant) {
     restaurantTable.innerHTML += `
       <tr class="collapsing">
-        <th>${rest.restaurant}</th>
+        <th><a id="test">${rest.restaurant}</a></th>
         <th>${rest.type}</th>
       </tr>
   `
@@ -57,7 +92,7 @@ searchByName.addEventListener('keyup', () => {
   xhr.send();
 
   let restaurantSearch = searchByName.value;
-  
+
   const searchRestaurant = (xhrRestaurant) => {
     let userFilter = [];
     for (xhrRestaurants of xhrRestaurant)
@@ -70,46 +105,155 @@ searchByName.addEventListener('keyup', () => {
 
 });
 
-const filterRest = (type) =>{
+const filterRest = (type, lugartabla) => {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', './data/restaurant.json', true);
   xhr.onload = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
       let xhrRestaurant = JSON.parse(xhr.responseText);
-      filterByType(xhrRestaurant);
+      filterByType(xhrRestaurant, lugartabla);
     }
   }
   xhr.send();
 
-  const filterByType = (xhrRestaurant) => {
-      let restFilter = [];
-      for (xhrRestaurants of xhrRestaurant)
-        if (xhrRestaurants.type.includes(type)) {
-          restFilter.push(xhrRestaurants);
-        }
-      for (rest of restFilter) {
-        tableAmazon.innerHTML = `
-        <tr class="collapsing">
-          <th>${rest.restaurant}</th>
-          <th>${rest.type}</th>
-        </tr>
-    `
+  const filterByType = (xhrRestaurant, trPlace) => {
+    trPlace.innerHTML = ``
+    let restFilter = [];
+    for (xhrRestaurants of xhrRestaurant)
+      if (xhrRestaurants.type.includes(type)) {
+        restFilter.push(xhrRestaurants);
       }
-      ;
-  console.log(restFilter);
-      return restFilter;
+    for (rest of restFilter) {
+      trPlace.innerHTML += `
+      <tr class="collapsing">
+      <th>${rest.restaurant}
+      <button class="ui button yellow create_btn" type="button" id="${rest.restaurant}">Create</button>
+	  <div class="ui modal test">
+  <i class="close icon"></i>
+  <div class="header">
+    Profile Picture
+  </div>
+  <div class="image content">
+    <div class="ui medium image">
+      <img src="https://semantic-ui.com/images/avatar2/large/rachel.png">
+    </div>
+    <div class="description">
+      <div class="ui header">We've auto-chosen a profile image for you.</div>
+      <p>We've grabbed the following image from the <a href="https://www.gravatar.com" target="_blank">gravatar</a> image associated with your registered e-mail address.</p>
+      <p>Is it okay to use this photo?</p>
+    </div>
+  </div>
+  <div class="actions">
+    <div class="ui black deny button">
+      Nope
+    </div>
+    <div class="ui positive right labeled icon button">
+      Yep, that's me
+      <i class="checkmark icon"></i>
+    </div>
+  </div>
+</div>
+      </th>
+      <th>${rest.type}</th>
+    </tr>
+    `
+    }
+let idRest = rest.restaurant
+    $(function(){
+      $("#"+idRest).click(function(){
+        $(".test").modal('show');
+      });
+      $(".test").modal({
+        closable: true
+      });
+    });
+    return restFilter;
   }
 }
 
-
 amazonRest.addEventListener('click', () => {
   const type = 'Amazónica'
-  filterRest(type);
+  const trPlace = tableAmazon;
+  filterRest(type, trPlace);
 })
 
+americanRest.addEventListener('click', () => {
+  const type = 'Americana'
+  const trPlace = tableAmerican;
+  filterRest(type, trPlace);
+})
 
+meatAndGrillRest.addEventListener('click', () => {
+  const type = 'Carnes y Parrillas'
+  const trPlace = tableMeatAndGrill;
+  filterRest(type, trPlace);
+})
 
+chifaRest.addEventListener('click', () => {
+  const type = 'Chifa'
+  const trPlace = tableChifa;
+  filterRest(type, trPlace);
+})
 
+fusionRest.addEventListener('click', () => {
+  const type = 'Fusión'
+  const trPlace = tableFusion;
+  filterRest(type, trPlace);
+})
+
+indiaRest.addEventListener('click', () => {
+  const type = 'India'
+  const trPlace = tableIndia;
+  filterRest(type, trPlace);
+})
+
+internationalRest.addEventListener('click', () => {
+  const type = 'Internacional'
+  const trPlace = tableInternational;
+  filterRest(type, trPlace);
+})
+
+italianRest.addEventListener('click', () => {
+  const type = 'Italiana'
+  const trPlace = tableItalian;
+  filterRest(type, trPlace);
+})
+
+nikkeiRest.addEventListener('click', () => {
+  const type = 'Nikkei / Japonesa'
+  const trPlace = tableNikkei;
+  filterRest(type, trPlace);
+})
+
+peruvianRest.addEventListener('click', () => {
+  const type = 'Peruana'
+  const trPlace = tablePeruvian;
+  filterRest(type, trPlace);
+})
+
+fishRest.addEventListener('click', () => {
+  const type = 'Pescados y Mariscos'
+  const trPlace = tableFish;
+  filterRest(type, trPlace);
+})
+
+snackRest.addEventListener('click', () => {
+  const type = 'Piqueos'
+  const trPlace = tableSnack;
+  filterRest(type, trPlace);
+})
+
+pizzaRest.addEventListener('click', () => {
+  const type = 'Pizzería'
+  const trPlace = tablePizza;
+  filterRest(type, trPlace);
+})
+
+thaiRest.addEventListener('click', () => {
+  const type = 'Thai'
+  const trPlace = tableThai;
+  filterRest(type, trPlace);
+})
 
 //Orden A-Z
 window.onload = () => {
